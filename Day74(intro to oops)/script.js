5311b5352b8568278fceb0e0573d87aed4772244
Prabhat Narayan Payasi <142197070+PrabhatNarayanPayasi@users.noreply.github.com>
2025-06-23 07:40:56 +0530

    Add files via upload

---
+++ Day74(intro to oops)/script.js
@@ -0,0 +1,25 @@
+// oops ek programming paradigm hai jo class aur objects per based hota hai. 
+// js me oops ka use code ko modular reusable aur maintable banane ke liye kiya jata hai .
+
+// class
+// ye ek blueprint type ka hai jisme method hote hai 
+
+// object :-new keyword ka use karke class ka obj bnaya jata hai . 
+// isme  propertie aur method hote hai jo class se derive hote hai . 
+
+
+// abstraction
+// iska matlb hai ki  cllass ki internal working ko chhupana aur sirf essential features ko expose karna 
+// js me abstraction nhi hote hainlekin getter setter aur private method
+// ka use karke ham abstraction ko immplement kar skte hai . 
+
+
+// Polymorphism:
+
+// Polymorphism ka matlab hai ki ek method ya function ka multiple behaviors ho sakte hain, yani ek method ko different contexts mein alag-alag tarike se implement kiya ja sakta hai.
+// JavaScript mein polymorphism ko method overriding ya method overloading ke through achieve kiya jata hai.
+
+// Encapsulation 
+// iska matlb hota hai ki data ko private aur protected rakhna aur us private ya protected data
+// ko access karne ke liye method provide karna  . 
+// getter aur setter ka use kar skte hai encapsulation ko handle
